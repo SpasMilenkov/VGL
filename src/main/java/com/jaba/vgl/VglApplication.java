4ece93b7078118cff9c026d3cc77eba43d1e9fcc
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-20 16:34:25 +0200

    Added missing files.

---
+++ src/main/java/com/jaba/vgl/VglApplication.java
@@ -0,0 +1,13 @@
+package com.jaba.vgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VglApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VglApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaba/vgl/VglApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaba.vgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VglApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
