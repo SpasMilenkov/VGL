f171f62ffd8afb2f91d50f79900a0bf2b8afe779
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-20 18:41:15 +0200

    Added Game repository.

---
+++ src/main/java/com/jaba/vgl/repositories/GameRepository.java
@@ -0,0 +1,24 @@
+package com.jaba.vgl.repositories;
+
+import com.jaba.vgl.models.entities.Game;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface GameRepository {
+
+    Optional<Game> findGameById(Long id);
+
+    Optional<Game> findGameByName(String name);
+
+    Optional<Game> findGameByNameAndCompany(String name, String company);
+
+    void updateGame(Long id, String name, String description, Float rating, String genre, String company, String studio, Boolean isFavourite, String releaseDate);
+
+    void createGame(Game game);
+
+    int deleteGameById(Long id);
+
+    void truncate();
+}
