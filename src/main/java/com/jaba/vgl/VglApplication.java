5a745bfe6d036bb9239519bde9fd234fde2b4cfd
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-20 16:44:18 +0200

    Configurated app class.

--- src/main/java/com/jaba/vgl/VglApplication.java
+++ src/main/java/com/jaba/vgl/VglApplication.java
@@ -2,8 +2,23 @@ package com.jaba.vgl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.QueryLookupStrategy;
 
 @SpringBootApplication
+@EnableJpaRepositories(
+		basePackages = "com.jaba.vgl.repositories"
+)
+@EnableFeignClients(
+		clients = {
+			//TODO: add steam client...
+		}
+)
+@ConfigurationPropertiesScan
+//@EnableConfigurationProperties
 public class VglApplication {
 
 	public static void main(String[] args) {
