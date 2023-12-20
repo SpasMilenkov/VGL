30af0ab4e37675b3783ed6358f4404d84b41e357
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-20 18:41:23 +0200

    Added Game details repository.

---
+++ src/main/java/com/jaba/vgl/repositories/GameDetailsRepository.java
@@ -0,0 +1,25 @@
+package com.jaba.vgl.repositories;
+
+import com.jaba.vgl.models.entities.Game;
+import com.jaba.vgl.models.entities.GameDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface GameDetailsRepository {
+
+    Optional<GameDetails> findGameDetailsById(Long id);
+
+    Optional<GameDetails> findGameDetailsByName(String name);
+
+    Optional<GameDetails> findGameDetailsByNameAndCompany(String name, String company);
+
+    void updateGameDetails(Long id, String name, String description, Float rating, String genre, String company, String studio, Boolean isFavourite, String releaseDate);
+
+    void createGameDetails(GameDetails game);
+
+    int deleteGameDetailsById(Long id);
+
+    void truncate();
+}
