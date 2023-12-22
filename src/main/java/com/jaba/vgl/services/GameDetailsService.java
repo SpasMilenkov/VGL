8bc4719ae1a988646ca38848320aa05f5fe8a201
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-22 16:06:31 +0200

    Added GameDetails service and impl.

--- src/main/java/com/jaba/vgl/services/GameDetailsService.java
+++ src/main/java/com/jaba/vgl/services/GameDetailsService.java
@@ -1,20 +1,18 @@
 package com.jaba.vgl.services;
 
-import com.jaba.vgl.models.entities.GameDetails;
-
-import java.util.List;
+import com.jaba.vgl.models.dto.GameDetailsDto;
 
 public interface GameDetailsService {
 
-    List<GameDetails> getGameDetails(Long id);
+    GameDetailsDto getGameDetails(Long id);
 
-    List<GameDetails> getGameDetails(String name);
+    GameDetailsDto getGameDetails(String name);
 
-    List<GameDetails> getGameDetails(String name, String company);
+    GameDetailsDto getGameDetails(String name, String company);
 
-    void updateGameDetails(GameDetails game);
+    void updateGameDetails(GameDetailsDto gameDetailsDto);
 
-    void createGameDetails(GameDetails game);
+    void createGameDetails(GameDetailsDto gameDetailsDto);
 
     int deleteGameDetails(Long id);
 
