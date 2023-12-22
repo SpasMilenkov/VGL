534a0b2dafc81cb6e98e1696202cb63003b6d502
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-22 21:37:32 +0200

    Create ReviewRepository

---
+++ src/main/java/com/jaba/vgl/repositories/ReviewRepository.java
@@ -0,0 +1,14 @@
+package com.jaba.vgl.repositories;
+
+import com.jaba.vgl.models.entities.Review;
+import com.jaba.vgl.repositories.custom.ReviewRepositoryCustom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
+
+    //Fetch by user id in GameDetailsRepo...
+
+    int deleteReviewById(Long id);
+
+}
