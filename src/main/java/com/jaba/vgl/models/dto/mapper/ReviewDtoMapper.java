7e1732eb706e04e164ec3bd6b73ce2a4eceb5882
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-22 17:28:50 +0200

    Create ReviewDtoMapper

---
+++ src/main/java/com/jaba/vgl/models/dto/mapper/ReviewDtoMapper.java
@@ -0,0 +1,19 @@
+package com.jaba.vgl.models.dto.mapper;
+
+import com.jaba.vgl.models.dto.ReviewDto;
+import com.jaba.vgl.models.entities.Review;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class ReviewDtoMapper implements Function<Review, ReviewDto> {
+    @Override
+    public ReviewDto apply(Review review) {
+        return new ReviewDto(
+                review.getTitle(),
+                review.getText(),
+                review.getRating()
+        );
+    }
+}
