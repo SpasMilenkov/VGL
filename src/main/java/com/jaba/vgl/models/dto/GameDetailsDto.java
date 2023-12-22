f314b3a6233d6b7dc8977fe8cb2b0ac431528073
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-22 16:04:18 +0200

    Added toEntity converter in GameDetailsDto.

--- src/main/java/com/jaba/vgl/models/dto/GameDetailsDto.java
+++ src/main/java/com/jaba/vgl/models/dto/GameDetailsDto.java
@@ -1,11 +1,15 @@
 package com.jaba.vgl.models.dto;
 
 import com.jaba.vgl.models.GameGenre;
+import com.jaba.vgl.models.entities.Game;
+import com.jaba.vgl.models.entities.GameDetails;
 
 import java.util.List;
 
 public record GameDetailsDto(
         String name,
+
+        String description,
 
         Float rating,
 
@@ -23,4 +27,21 @@ public record GameDetailsDto(
 
         List<GameDto> moreLikeThisList
         ) {
+) {
+
+    public GameDetails toEntity() {
+        GameDetails gameDetails = new GameDetails();
+        GameDetailsDto dto = this;
+
+        gameDetails.setName(dto.name());
+        gameDetails.setDescription(dto.description());
+        gameDetails.setRating(dto.rating());
+        gameDetails.setGenre(dto.genre());
+        gameDetails.setCompany(dto.company());
+        gameDetails.setStudio(dto.studio());
+        gameDetails.setIsFavourite(dto.isFavourite());
+        gameDetails.setReleaseDate(dto.releaseDate());
+
+        return gameDetails;
+    }
 }
