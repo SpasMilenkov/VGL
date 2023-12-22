5ba3d030bde1734af0cd4b4522dfca519ec3d104
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-22 16:04:50 +0200

    Added GameDetailsDto mapper.

---
+++ src/main/java/com/jaba/vgl/models/dto/mapper/GameDetailsDtoMapper.java
@@ -0,0 +1,28 @@
+package com.jaba.vgl.models.dto.mapper;
+
+import com.jaba.vgl.models.dto.GameDetailsDto;
+import com.jaba.vgl.models.entities.GameDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class GameDetailsDtoMapper implements Function<GameDetails, GameDetailsDto> {
+
+    private final ReviewDtoMapper reviewDtoMapper;
+
+
+    @Override
+    public GameDetailsDto apply(GameDetails game) {
+        return new GameDetailsDto(
+                game.getName(),
+                game.getDescription(),
+                game.getRating(),
+                game.getGenre(),
+                game.getCompany(),
+                game.getStudio(),
+                game.getIsFavourite(),
+                game.getReleaseDate(),
+    }
+}
