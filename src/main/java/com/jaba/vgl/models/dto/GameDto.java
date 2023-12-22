9e116866371997a2ceb3343f6d162504cc3a463a
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-22 15:26:55 +0200

    Added toEntity convertor in GameDto. Added missing description parameter.

--- src/main/java/com/jaba/vgl/models/dto/GameDto.java
+++ src/main/java/com/jaba/vgl/models/dto/GameDto.java
@@ -1,9 +1,12 @@
 package com.jaba.vgl.models.dto;
 
 import com.jaba.vgl.models.GameGenre;
+import com.jaba.vgl.models.entities.Game;
 
 public record GameDto(
         String name,
+
+        String description,
 
         Float rating,
 
@@ -17,4 +20,18 @@ public record GameDto(
 
         String releaseDate
 ) {
+    public Game toEntity() {
+        Game game = new Game();
+        GameDto dto = this;
+
+        game.setName(dto.name());
+        game.setDescription(dto.description());
+        game.setRating(dto.rating());
+        game.setGenre(dto.genre());
+        game.setCompany(dto.company());
+        game.setStudio(dto.studio());
+        game.setIsFavourite(dto.isFavourite());
+        game.setReleaseDate(dto.releaseDate());
+        return game;
+    }
 }
