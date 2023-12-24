8d6c5d2d38ffd08a1b476387e6aec48b35595394
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-12-24 17:43:32 +0200

    Wired user service impl with review repository and new user service functionalities.

--- backend/src/main/java/com/jaba/vgl/services/UserService.java
+++ backend/src/main/java/com/jaba/vgl/services/UserService.java
@@ -1,9 +1,17 @@
 package com.jaba.vgl.services;
 
+import com.jaba.vgl.models.dto.ReviewDto;
 import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.List;
 
 public interface UserService {
 
     UserDetailsService userDetailsService();
 
+    List<ReviewDto> getUserReviews(Long userId);
+
+    void updateUserReview(ReviewDto reviewDto);
+
+    void deleteUserReview(Long reviewId);
 }
--- backend/src/main/java/com/jaba/vgl/services/impl/UserServiceImpl.java
+++ backend/src/main/java/com/jaba/vgl/services/impl/UserServiceImpl.java
@@ -1,7 +1,12 @@
 package com.jaba.vgl.services.impl;
 
 
+import com.jaba.vgl.models.dto.ReviewDto;
+import com.jaba.vgl.models.dto.mapper.ReviewDtoMapper;
+import com.jaba.vgl.models.entities.Review;
 import com.jaba.vgl.repositories.UserRepository;
+import com.jaba.vgl.repositories.impl.ReviewRepositoryImpl;
+import com.jaba.vgl.repositories.impl.UserRepositoryImpl;
 import com.jaba.vgl.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,16 +15,19 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import com.jaba.vgl.models.entities.User;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository;
+    private final UserRepositoryImpl userRepository;
+    private final ReviewRepositoryImpl reviewRepository;
+    private final ReviewDtoMapper reviewDtoMapper;
 
     @Override
-    public UserDetailsService userDetailsService(){
+    public UserDetailsService userDetailsService() {
         return new UserDetailsService() {
             @Override
             public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -28,9 +36,29 @@ public class UserServiceImpl implements UserService {
             }
         };
     }
+
     public User loadUserByRefreshToken(String refreshToken) {
         Optional<User> optionalUser = userRepository.findByRefreshToken(refreshToken);
         return optionalUser.orElse(null);
     }
 
+    @Override
+    public List<ReviewDto> getUserReviews(Long userId) {
+        return userRepository.getUserReviews(userId)
+                .stream()
+                .map(reviewDtoMapper)
+                .toList();
+    }
+
+    @Override
+    public void updateUserReview(ReviewDto reviewDto) {
+        Review review = reviewDto.toEntity();
+
+        reviewRepository.updateReview(review.getId(), review.getGameId(), review.getTitle(), review.getText(), review.getRating());
+    }
+
+    @Override
+    public void deleteUserReview(Long reviewId) {
+        reviewRepository.deleteReviewById(reviewId);
+    }
 }
