df448005b10f1aa2e5e9ea9ced12c2d8eb3cef5c
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-29 15:18:05 +0200

    Create NewsItemDto

---
+++ backend/src/main/java/com/jaba/vgl/models/dto/NewsItemDto.java
@@ -0,0 +1,15 @@
+package com.jaba.vgl.models.dto;
+
+import lombok.Data;
+
+@Data
+public class NewsItemDto {
+    private String id;
+    private String title;
+    private String author;
+    private String contents;
+    private String feedLabel;
+    private long date;
+    private String feedName;
+    private int appid;
+}
