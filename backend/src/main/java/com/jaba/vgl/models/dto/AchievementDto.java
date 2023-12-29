cd5dab33905a18ba4b27b1f5dc6fcaf91402d70d
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-12-29 15:17:56 +0200

    Create AchievementDto

---
+++ backend/src/main/java/com/jaba/vgl/models/dto/AchievementDto.java
@@ -0,0 +1,10 @@
+package com.jaba.vgl.models.dto;
+
+import lombok.Data;
+
+@Data
+public class AchievementDto {
+    private String apiName;
+    private boolean achieved;
+    private String name;
+}
