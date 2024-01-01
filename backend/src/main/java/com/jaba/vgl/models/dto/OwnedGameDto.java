246b1c4c7342a0ab855e0f0b637f48085dcf12ec
Spas Milenkov <spasmilenkov123@gmail.com>
2024-01-01 18:26:20 +0200

    Separate the owned game dto from the description dto due to different api responses

--- backend/src/main/java/com/jaba/vgl/models/dto/OwnedGameDto.java
+++ backend/src/main/java/com/jaba/vgl/models/dto/OwnedGameDto.java
@@ -2,16 +2,14 @@ package com.jaba.vgl.models.dto;
 
 import lombok.Data;
 
-import java.util.Date;
-import java.util.List;
-
 @Data
 public class OwnedGameDto {
     private int gameId;
     private String name;
     private int playtimeForever;
     private String bannerUrl;
-    private String shortDescription;
-    private List<String> genres;
-    private Date releaseDate;
+    //the steam api has several versions some games haven't uploaded banner url
+    //the frontend should fall back to this image since it is used by the older games
+    //that haven't updated ¯\_(ツ)_/¯
+    private String headerUrl;
 }
