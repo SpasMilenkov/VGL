e923ec2ba4cfd31507e5189a56f51d34170adde9
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-01-03 22:20:32 +0200

    Create BecomePart component

---
+++ frontend/src/components/landing/BecomePart.tsx
@@ -0,0 +1,18 @@
+const BecomePart = () => {
+  return (
+    <section id="section-become-part">
+        <img
+          className="heropage-background"
+          src='src/assets/images/landing/gifche2.gif'
+          alt=""
+        />
+        <div className="becomepart-background-overlay"></div>
+        <div className="section-hero-title">Become a part of our community</div>
+        <div className="section-hero-buttons">
+          <button className="button-join-us sign-up">Join us &nbsp; &#8599;</button>
+        </div>
+    </section>
+  )
+}
+
+export default BecomePart
