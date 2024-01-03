4644f0450b93dbab14c9597cf8ed63677b7fc70d
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2024-01-03 20:25:34 +0200

    Finish AccessForm

--- frontend/src/components/AccessForm.tsx
+++ frontend/src/components/AccessForm.tsx
@@ -1,88 +1,97 @@
-import { useState } from "react"
-import "../styles/global.css"
+import React, { useState } from "react";
 
-const AccessForm = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [steamId, setSteamId] = useState('');
-
-  const handleSubmit = () =>{
-    
-  }
-
-  return (
-    <div className="
-      flex justify-center 
-      items-center bg-gray-15 
-      gap-4 p-4 white-98 w-1/2 rounded-2xl h-4/6">
-      <form className="
-        flex flex-col justify-center 
-        items-center bg-gray-15 
-        gap-4 p-4 white-98 w-1/2">
-        <h2 className="form-h2 white-98">Login</h2>
-        <input 
-          value={email} 
-          className="form-input"
-          onChange={(e) => setEmail(e.target.value)} 
-          type="email" 
-          placeholder="Email" />
-
-        <input 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
-          className="form-input"
-          type="password" 
-          placeholder="Password" />
-
-        <button 
-          onClick={handleSubmit}
-          className="form-button">
-            Login
-        </button>
-      </form>
-      <form className="
-      flex flex-col justify-center 
-      items-center bg-gray-15 
-      gap-4 p-4 white-98 w-1/2">
-        <h2 className="form-h2 white-98">Register</h2>
-      
-        <input 
-          value={name} 
-          onChange={(e) => setName(e.target.value)} 
-          className="form-input"
-          type="text" 
-          placeholder="Name" />
-
-        <input 
-          value={email} 
-          onChange={(e) => setEmail(e.target.value)} 
-          className="form-input"
-          type="email" 
-          placeholder="Email" />
-
-        <input 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
-          className="form-input"
-          type="password" 
-          placeholder="Password" />
-
-        <input 
-          value={steamId} 
-          onChange={(e) => setSteamId(e.target.value)} 
-          className="form-input"
-          type="text" 
-          placeholder="SteamID" />     
-
-        <button 
-          onClick={handleSubmit}
-          className="form-button">
-            Sign Up
-        </button>
-      </form>
-    </div>
-  )
+interface Props {
+  isLoginOn: boolean;
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default AccessForm
+const AccessForm: React.FC<Props> = ({ isLoginOn, setState }: Props) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [steamId, setSteamId] = useState("");
+
+  const handleSubmit = () => {};
+  return (
+    <div className="login-form-wrapper">
+      <div
+        className={`flex justify-center items-center bg-gray-15 white-98 w-[60%] rounded-2xl h-[70%] ${
+          !isLoginOn ? "flex-row-reverse" : ""
+        }`}
+      >
+        <form
+          className="
+      flex flex-col justify-center 
+      items-center bg-gray-15 
+      gap-4 p-4 white-98 w-1/2
+      rounded-2xl"
+        >
+          <h2 className="form-h2 white-98">
+            {isLoginOn ? "Login" : "Register"}
+          </h2>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-input"
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-input"
+            type="password"
+            placeholder="Password"
+          />
+          {!isLoginOn && (
+            <>
+              <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="form-input"
+                type="text"
+                placeholder="Name"
+              />
+              <input
+                value={steamId}
+                onChange={(e) => setSteamId(e.target.value)}
+                className="form-input"
+                type="text"
+                placeholder="SteamID"
+              />
+            </>
+          )}
+
+          <button onClick={handleSubmit} className="form-button">
+            {isLoginOn ? "Login" : "Sign Up"}
+          </button>
+        </form>
+        {/* Unselected Container */}
+        <div
+          className={`flex justify-center items-center flex-col gap-[15%] bg-gradient-to-tr from-[#FF5C00] to-[#FF9838] w-1/2 h-full  ${
+            isLoginOn ? "rounded-r-2xl" : "rounded-l-2xl"
+          }`}
+        >
+          <div className="flex flex-col justify-center items-center w-[65%] gap-4">
+            <h2 className="font-medium text-5xl text-center">
+              {isLoginOn ? "Hello!" : "Welcome back!"}
+            </h2>
+            <h3 className="font-normal text-xl text-center">
+              {isLoginOn
+                ? "Are you here for the first time? Start your journey with us."
+                : "To keep connected with us please login with your pesonal info."}
+            </h3>
+          </div>
+          <button
+            className="unselected-button"
+            onClick={() => setState(!isLoginOn)}
+          >
+            {isLoginOn ? "Sign Up" : "Login"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AccessForm;
