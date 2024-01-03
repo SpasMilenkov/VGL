8c0f60c11b1947d54bfd52bbb353cc7a9a5a09a8
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2024-01-03 23:30:59 +0200

    add becomepart form popup

--- frontend/src/components/landing/BecomePart.tsx
+++ frontend/src/components/landing/BecomePart.tsx
@@ -1,18 +1,30 @@
-const BecomePart = () => {
+interface Props {
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
+  setFormIsActive: React.Dispatch<React.SetStateAction<boolean>>;
+}
+const BecomePart: React.FC<Props> = ({ setState, setFormIsActive }: Props) => {
   return (
     <section id="section-become-part">
-        <img
-          className="heropage-background"
-          src='src/assets/images/landing/gifche2.gif'
-          alt=""
-        />
-        <div className="becomepart-background-overlay"></div>
-        <div className="section-hero-title">Become a part of our community</div>
-        <div className="section-hero-buttons">
-          <button className="button-join-us sign-up">Join us &nbsp; &#8599;</button>
-        </div>
+      <img
+        className="heropage-background"
+        src="src/assets/images/landing/gifche2.gif"
+        alt=""
+      />
+      <div className="becomepart-background-overlay"></div>
+      <div className="section-hero-title">Become a part of our community</div>
+      <div className="section-hero-buttons">
+        <button
+          onClick={() => {
+            setState(false);
+            setFormIsActive(true);
+          }}
+          className="button-join-us sign-up"
+        >
+          Join us &nbsp; &#8599;
+        </button>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default BecomePart
+export default BecomePart;
