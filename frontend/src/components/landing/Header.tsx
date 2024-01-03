1e1a77ffe8a0a26ad97ff973feeac98ee9058e64
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2024-01-03 23:30:50 +0200

    add header form popup

--- frontend/src/components/landing/Header.tsx
+++ frontend/src/components/landing/Header.tsx
@@ -1,19 +1,40 @@
 interface Props {
   setState: React.Dispatch<React.SetStateAction<boolean>>;
+  setFormIsActive: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Header: React.FC<Props> = ({ setState }: Props) => {
+const Header: React.FC<Props> = ({ setState, setFormIsActive }: Props) => {
   return (
     <div className="header-container">
       <div className="header">
         <div className="left-section">
-          <img src='src/assets/images/landing/logo.png' alt="Website Logo" className="header-logo"/>
+          <img
+            src="src/assets/images/landing/logo.png"
+            alt="Website Logo"
+            className="header-logo"
+          />
 
           <div className="title-text">The Game List</div>
         </div>
         <div className="right-section">
-          <button onClick={() => setState(true)} className="button-login-signup login">Login</button>
-          <button onClick={() => setState(false)} className="button-login-signup sign-up">Sign Up</button>
+          <button
+            onClick={() => {
+              setState(true);
+              setFormIsActive(true);
+            }}
+            className="button-login-signup login"
+          >
+            Login
+          </button>
+          <button
+            onClick={() => {
+              setState(false);
+              setFormIsActive(true);
+            }}
+            className="button-login-signup sign-up"
+          >
+            Sign Up
+          </button>
         </div>
       </div>
     </div>
