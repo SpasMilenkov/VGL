3510d221147635b5879cbacf3988ae6f371f860d
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-01-03 22:20:52 +0200

    Create LandingReviews component

---
+++ frontend/src/components/landing/LandingReviews.tsx
@@ -0,0 +1,78 @@
+const LandingReviews = () => {
+  return (
+    <section id="section-reviews">
+        <div className="review-container">
+          <div className="reviews-text">20 000+ reviews</div>
+          <div className="review-card">
+            <img
+              src="src/assets/images/landing/game1.jpg"
+              alt=""
+              className="review-img"
+            />
+            <div className="review-card-text glass">Olympic57</div>
+          </div>
+          <div className="review-card">
+            <img
+              src="src/assets/images/landing/game2.jpg"
+              alt=""
+              className="review-img"
+            />
+            <div className="review-card-text glass">Beastry</div>
+          </div>
+          <div className="review-card">
+            <img
+              src="src/assets/images/landing/game3.jpg"
+              alt=""
+              className="review-img"
+            />
+            <div className="review-card-text glass">Stef</div>
+          </div>
+          <div className="review-card">
+            <img
+              src="src/assets/images/landing/game4.webp"
+              alt=""
+              className="review-img"
+            />
+            <div className="review-card-text glass">Gali</div>
+          </div>
+        </div>
+        <div className="review-container">
+          <div className="review-card">
+            <img
+              src="src/assets/images/landing/game7.webp"
+              alt=""
+              className="review-img"
+            />
+            <div className="review-card-text glass">Hyport77</div>
+          </div>
+          <div className="review-card">
+            <img
+              src="src/assets/images/landing/game6.jpg"
+              alt=""
+              className="review-img"
+            />
+            <div className="review-card-text glass">Featurelynk</div>
+          </div>
+          <div className="review-card">
+            <img
+              src="src/assets/images/landing/game8.jpg"
+              alt=""
+              className="review-img"
+            />
+            <div className="review-card-text glass">Anderha38</div>
+          </div>
+          <div className="review-card">
+            <img
+              src="src/assets/images/landing/game9.jpg"
+              alt=""
+              className="review-img"
+            />
+            <div className="review-card-text glass">iiChelagii</div>
+          </div>
+          <div className="reviews-text">From trusted users</div>
+        </div>
+      </section>
+  )
+}
+
+export default LandingReviews
