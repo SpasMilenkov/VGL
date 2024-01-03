c0032823eb12875b6955d899d60144fdd228bc3f
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2024-01-03 22:44:43 +0200

    refactor landing reviews for reusability

--- frontend/src/components/landing/LandingReviews.tsx
+++ frontend/src/components/landing/LandingReviews.tsx
@@ -1,78 +1,62 @@
+import LandingReviewCard from "./LandingReviewCard";
+
 const LandingReviews = () => {
+  const data = [
+    {
+      imgUrl: "src/assets/images/landing/game1.jpg",
+      reviewerName: "Olympic57",
+    },
+    {
+      imgUrl: "src/assets/images/landing/game2.jpg",
+      reviewerName: "Beastry",
+    },
+    {
+      imgUrl: "src/assets/images/landing/game3.jpg",
+      reviewerName: "Stef",
+    },
+    {
+      imgUrl: "src/assets/images/landing/game4.webp",
+      reviewerName: "Gali",
+    },
+    {
+      imgUrl: "src/assets/images/landing/game7.webp",
+      reviewerName: "Hyport77",
+    },
+    {
+      imgUrl: "src/assets/images/landing/game6.jpg",
+      reviewerName: "Featurelynk",
+    },
+    {
+      imgUrl: "src/assets/images/landing/game8.jpg",
+      reviewerName: "Anderha38",
+    },
+    {
+      imgUrl: "src/assets/images/landing/game9.jpg",
+      reviewerName: "iiChelagii",
+    },
+  ];
   return (
     <section id="section-reviews">
-        <div className="review-container">
-          <div className="reviews-text">20 000+ reviews</div>
-          <div className="review-card">
-            <img
-              src="src/assets/images/landing/game1.jpg"
-              alt=""
-              className="review-img"
-            />
-            <div className="review-card-text glass">Olympic57</div>
-          </div>
-          <div className="review-card">
-            <img
-              src="src/assets/images/landing/game2.jpg"
-              alt=""
-              className="review-img"
-            />
-            <div className="review-card-text glass">Beastry</div>
-          </div>
-          <div className="review-card">
-            <img
-              src="src/assets/images/landing/game3.jpg"
-              alt=""
-              className="review-img"
-            />
-            <div className="review-card-text glass">Stef</div>
-          </div>
-          <div className="review-card">
-            <img
-              src="src/assets/images/landing/game4.webp"
-              alt=""
-              className="review-img"
-            />
-            <div className="review-card-text glass">Gali</div>
-          </div>
-        </div>
-        <div className="review-container">
-          <div className="review-card">
-            <img
-              src="src/assets/images/landing/game7.webp"
-              alt=""
-              className="review-img"
-            />
-            <div className="review-card-text glass">Hyport77</div>
-          </div>
-          <div className="review-card">
-            <img
-              src="src/assets/images/landing/game6.jpg"
-              alt=""
-              className="review-img"
-            />
-            <div className="review-card-text glass">Featurelynk</div>
-          </div>
-          <div className="review-card">
-            <img
-              src="src/assets/images/landing/game8.jpg"
-              alt=""
-              className="review-img"
-            />
-            <div className="review-card-text glass">Anderha38</div>
-          </div>
-          <div className="review-card">
-            <img
-              src="src/assets/images/landing/game9.jpg"
-              alt=""
-              className="review-img"
-            />
-            <div className="review-card-text glass">iiChelagii</div>
-          </div>
-          <div className="reviews-text">From trusted users</div>
-        </div>
-      </section>
-  )
-}
+      <div className="review-container">
+        <div className="reviews-text">20 000+ reviews</div>
+        {data.slice(0, 4).map((card) => (
+          <LandingReviewCard
+            imgUrl={card.imgUrl}
+            reviewerName={card.reviewerName}
+          ></LandingReviewCard>
+        ))}
+      </div>
+      <div className="review-container">
+        {data.slice(4, 8).map((card) => (
+          <LandingReviewCard
+            imgUrl={card.imgUrl}
+            reviewerName={card.reviewerName}
+          ></LandingReviewCard>
+        ))}
+        <div className="reviews-text">From trusted users</div>
+      </div>
+    </section>
+  );
+};
 
-export default LandingReviews
+export default LandingReviews;
