f4938c15bfc9a622aba0e728e146e5cab3929c57
Spas Milenkov <spasmilenkov123@gmail.com>
2024-01-04 19:07:25 +0200

    Add endpoint for updating steamId

--- backend/src/main/java/com/jaba/vgl/controllers/UserController.java
+++ backend/src/main/java/com/jaba/vgl/controllers/UserController.java
@@ -1,18 +1,26 @@
 package com.jaba.vgl.controllers;
 
 
+import com.jaba.vgl.models.dto.UserDto;
+import com.jaba.vgl.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
     @GetMapping
     public ResponseEntity<String> sayHello(){
         return ResponseEntity.ok("Bye world");
+    }
+
+    @PutMapping("update-steamId")
+    public ResponseEntity<UserDto> updateSteamId(@RequestParam String email, @RequestParam String steamId){
+        UserDto userDto = userService.updateSteamId(email, steamId);
+
+        return ResponseEntity.ok(userDto);
     }
 }
