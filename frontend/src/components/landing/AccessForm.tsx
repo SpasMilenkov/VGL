91c2142dc5732edd7d0a2c8649584d36c0fba0ab
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-01-04 15:27:14 +0200

    Refactor AccessForm to use the login and register components

--- frontend/src/components/landing/AccessForm.tsx
+++ frontend/src/components/landing/AccessForm.tsx
@@ -1,20 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import Register from "./Register";
+import Login from "./Login";
 
 interface Props {
   isLoginOn: boolean;
   setState: React.Dispatch<React.SetStateAction<boolean>>;
   setIsActive: React.Dispatch<React.SetStateAction<boolean>>;
-}
+};
 
-const AccessForm: React.FC<Props> = ({
-  isLoginOn,
-  setState,
-  setIsActive,
-}: Props) => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [steamId, setSteamId] = useState("");
+const AccessForm: React.FC<Props> = ({isLoginOn, setState, setIsActive}) => {
 
   useEffect(() => {
     document.body.classList.add("overflow-y-hidden");
@@ -24,63 +18,21 @@ const AccessForm: React.FC<Props> = ({
     };
   }, []);
 
-  const handleSubmit = () => {
-    setIsActive(false);
-  };
   return (
     <div className={`login-form-wrapper`}>
       <div
-        className={`flex justify-center items-center bg-gray-15 white-98 w-[60%] rounded-2xl h-[70%] ${
+        className={`relative flex justify-center items-center bg-gray-15 white-98 w-[60%] rounded-2xl h-[70%] ${
           !isLoginOn ? "flex-row-reverse" : ""
         }`}
       >
-        <form
-          className="
-      flex flex-col justify-center 
-      items-center bg-gray-15 
-      gap-4 p-4 white-98 w-1/2
-      rounded-2xl"
-        >
-          <h2 className="form-h2 white-98">
-            {isLoginOn ? "Login" : "Register"}
-          </h2>
-          <input
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="form-input"
-            type="email"
-            placeholder="Email"
-          />
-          <input
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="form-input"
-            type="password"
-            placeholder="Password"
-          />
-          {!isLoginOn && (
-            <>
-              <input
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="form-input"
-                type="text"
-                placeholder="Name"
-              />
-              <input
-                value={steamId}
-                onChange={(e) => setSteamId(e.target.value)}
-                className="form-input"
-                type="text"
-                placeholder="SteamID"
-              />
-            </>
-          )}
+        <img 
+          onClick={() => setIsActive(false)} 
+          className="login-close" 
+          src="src/assets/images/landing/close-icon.png" alt="Close">
+        </img>
 
-          <button onClick={handleSubmit} className="form-button">
-            {isLoginOn ? "Login" : "Sign Up"}
-          </button>
-        </form>
+        {isLoginOn ? <Login/> : <Register/>}
+
         {/* Unselected Container */}
         <div
           className={`flex justify-center items-center flex-col gap-[15%] bg-gradient-to-tr from-[#FF5C00] to-[#FF9838] w-1/2 h-full  ${
