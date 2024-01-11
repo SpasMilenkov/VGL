17e9745dae378b38728039999a9486272b48bc3b
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2024-01-11 21:01:42 +0200

    update gamelistcard

--- frontend/src/components/gamelist/GameListCard.tsx
+++ frontend/src/components/gamelist/GameListCard.tsx
@@ -3,9 +3,12 @@ import type { Game } from '../../interfaces/Game';
 
 interface GameProps {
   game: Game
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setGameId: React.Dispatch<React.SetStateAction<number>>;
+  setGameName: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const GameListCard:React.FC<GameProps> = ({game}) => {
+const GameListCard:React.FC<GameProps> = ({game, setGameId, setGameName, setIsModalOpen}) => {
   const [imageUrl, setImageUrl] = useState(game.bannerUrl);
 
   useEffect(() =>{
@@ -16,8 +19,15 @@ const GameListCard:React.FC<GameProps> = ({game}) => {
     setImageUrl(game.headerUrl);
   };
 
+  const handleModalOpen = () =>{
+    setIsModalOpen(true);
+    setGameId(game.gameId);
+    setGameName(game.name);
+    document.body.classList.add("overflow-y-hidden");
+  }
+
   return (
-    <div className="card">
+    <div className="card relative">
       <div className="card-game-container">
         <img
           className="card-game-cover"
@@ -32,6 +42,12 @@ const GameListCard:React.FC<GameProps> = ({game}) => {
           <div className="card-game-studio">{game.studio}</div>
         </div>
       </div>
+      <img 
+        title='Add Review'
+        onClick={handleModalOpen}
+        className='w-[2rem] h-[2rem] cursor-pointer absolute right-6 top-6' 
+        src="src/assets/icons/review-icon.svg" 
+        alt="ReviewIcon" />
     </div>
   )
 }
