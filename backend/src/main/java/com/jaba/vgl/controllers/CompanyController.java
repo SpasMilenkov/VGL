f3cb7a539a1b50ff78f781eb8d5a06be200ff38f
Stefan Gavrailov <108361580+Maglata@users.noreply.github.com>
2024-01-11 19:53:12 +0200

    initial company controller

---
+++ backend/src/main/java/com/jaba/vgl/controllers/CompanyController.java
@@ -0,0 +1,30 @@
+package com.jaba.vgl.controllers;
+
+import com.jaba.vgl.models.dto.CompanyDto;
+import com.jaba.vgl.services.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/companies")
+public class CompanyController {
+
+    private final CompanyService companyService;
+
+    @Autowired
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CompanyDto> getCompany(@PathVariable Long id) {
+        return ResponseEntity.ok(companyService.getCompany(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> saveCompany(@RequestBody CompanyDto companyDto) {
+        companyService.saveCompany(companyDto);
+        return ResponseEntity.ok().build();
+    }
+}
